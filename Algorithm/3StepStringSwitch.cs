c9f1fa86e05e305b3c2ce2d6b221e3067ba82b2d
TAO <kevin.shantao@gmail.com>
2018-08-21 20:04:43 +0800

    三步翻转字符串
    简单实现三步翻转字符串算法 
    原理：字符串S 分解两部分 X Y 各部分单独翻转 X`T Y`T . 新串(X`T Y`T) `T整体翻转得结果 YX

---
+++ Algorithm/3StepStringSwitch.cs
@@ -0,0 +1,38 @@
+//三步翻转
+using System;
+
+namespace Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "I am a Student.";
+            string[] strArr = str.Split(new char[] { ' '});
+            string res = "";
+            foreach (string s in strArr)
+            {
+                //逐个翻转
+                res = res + " "+Swtich(s.ToCharArray(), 0, s.Length - 1);
+            }
+            //整体翻转
+            res = Swtich(res.Trim().ToCharArray(), 0, res.Trim().Length - 1);
+            Console.WriteLine(res);
+            Console.Read();
+        }
+
+        public static string Swtich(char[] carr, int start,int end)
+        {
+            while (start < end)
+            {
+                char c = carr[start];
+                carr[start] = carr[end];
+                carr[end] = c;
+                start++;
+                end--;
+            }
+            string s = new string(carr);
+            return s;
+        }
+    }
+}
